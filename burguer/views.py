01f6180511e402ae038def7a046daf73f83ee319
ilton <xlsonplay@gmail.com>
2023-01-06 22:52:02 -0300

    Rotas e views Ok

--- burguer/views.py
+++ burguer/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return HttpResponse("Ola Mundo")
+	return render(request, 'burguer/home.html')
+
+def detalhe_produto(request):
+	return render(request, 'burguer/produto.html')
